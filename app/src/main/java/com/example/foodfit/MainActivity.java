29707645293a6bacd43632a8860ec808e696439f
Shravan Jadhav <shravanjadhav041@gmail.com>
2025-07-21 23:06:31 +0530

    first change

--- app/src/main/java/com/example/foodfit/MainActivity.java
+++ app/src/main/java/com/example/foodfit/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//chaneg 3git
         };
 
     }
