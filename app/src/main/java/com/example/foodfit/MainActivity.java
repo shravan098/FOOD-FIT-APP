070dd79ecf87c4d2e4e3307246b8ebac04948eb3
Shravan Jadhav <shravanjadhav041@gmail.com>
2025-07-22 20:45:12 +0530

    4 commit

--- app/src/main/java/com/example/foodfit/MainActivity.java
+++ app/src/main/java/com/example/foodfit/MainActivity.java
@@ -8,10 +8,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//chaneg 3git
-        //fiofhakdfhoid
-        /fdefef
-        };
+
+        }
 
     }
 
