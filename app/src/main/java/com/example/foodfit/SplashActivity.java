c8d9126442f15a5c61a70d9b7dd800d52e4b3ab6
Shravan Jadhav <shravanjadhav041@gmail.com>
2025-07-26 20:56:39 +0530

    APP First Page

---
+++ app/src/main/java/com/example/foodfit/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.foodfit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 3500; // 3.5 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, FoodSearchActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_DELAY);
+    }
+}
